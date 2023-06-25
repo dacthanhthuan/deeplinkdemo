6df2a470d3932c9712f936974fd585c90c65782e
Harry Doan <doankhanhdong3105@gmail.com>
2023-06-25 19:36:48 +0700

    Fix selecTag default feature

--- App.tsx
+++ App.tsx
@@ -1,16 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from 'react';
 import 'react-native-gesture-handler';
-import { StyleSheet } from 'react-native'
-import Home from "./src/screens/Home";
-import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-import { createStackNavigator } from '@react-navigation/stack';
-import Search from "./src/screens/Search";
-import RecipeDetails from "./src/screens/RecipeDetails";
+import {StyleSheet} from 'react-native';
+import Home from './src/screens/Home';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Search from './src/screens/Search';
+import RecipeDetails from './src/screens/RecipeDetails';
 
 const Stack = createStackNavigator();
 
 const App = () => {
-
   const theme = {
     ...DefaultTheme,
     colors: {
@@ -21,22 +20,25 @@ const App = () => {
 
   return (
     <NavigationContainer theme={theme}>
-      <Stack.Navigator screenOptions={{ headerTitleAlign: 'center', headerShadowVisible: false }}>
+      <Stack.Navigator
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerShadowVisible: false,
+        }}>
         <Stack.Screen component={Home} name="Home" />
         <Stack.Screen component={Search} name="Search" />
         <Stack.Screen component={RecipeDetails} name="RecipeDetails" />
       </Stack.Navigator>
     </NavigationContainer>
-  )
-
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFFFFF',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
 });
 
